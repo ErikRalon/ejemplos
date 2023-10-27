4b88b4438e1a886233f3b0b024e1fc04c68e5655
ErikRalon <eralonh@miumg.edu.gt>
2023-10-26 20:39:57 -0600

    carfainicial

---
+++ Documents/NetBeansProjects/ejemplosgit/src/main/java/com/mycompany/ejemplosgit/Ejemplosgit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplosgit;
+
+/**
+ *
+ * @author Erik
+ */
+public class Ejemplosgit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
